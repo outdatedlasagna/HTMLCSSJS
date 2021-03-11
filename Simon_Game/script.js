da9f3e863c03fb49522f8b5076b653b6302ff8a3
Maria Corpus <mrnnacrps@gmail.com>
2021-03-11 23:56:31 +0800

    file updated

--- Simon_Game/script.js
+++ Simon_Game/script.js
@@ -1,8 +1,12 @@
-let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
+let buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
 
   let randomNumber = Math.floor(Math.random() * 4 + 1);
   console.log(randomNumber);
 }
+
+let randomChosenColour = 
+// use the randomNumber from step 2 to select a random colour from the buttonColours array.
